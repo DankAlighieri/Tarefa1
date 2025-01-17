4d435763c5f407383b31f2550ad1a701a846bbb3
Enzo <enzothi2@gmail.com>
2025-01-16 22:14:39 -0300

    Código para declarar a estrutura do keypad

---
+++ estrutura-keypad.c
@@ -0,0 +1,19 @@
+#include "pico/stdlib.h"
+
+// Definição dos pinos do teclado matricial
+#define ROWS 4
+#define COLS 4
+
+// Atualização dos pinos de acordo com as novas conexões
+const uint8_t row_pins[ROWS] = {8, 7, 6, 5}; // Pinos conectados às linhas R1, R2, R3, R4 const 
+const uint8_t col_pins[COLS] = {4, 3, 2, 1}; // Pinos conectados às colunas C1, C2, C3, C4
+
+// Mapa de teclas do teclado matricial
+const char key_map[ROWS][COLS] = {
+    {'1', '2', '3', 'A'},
+    {'4', '5', '6', 'B'},
+    {'7', '8', '9', 'C'},
+    {'*', '0', '#', 'D'}
+
+};
+
