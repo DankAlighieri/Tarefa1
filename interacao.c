25d76fe7a711e73b055d6bf875c4793faeadad18
Camila-RG <camilaramosgomes@gmail.com>
2025-01-18 19:31:29 -0300

    Alteração da main

--- interacao.c
+++ interacao.c
@@ -1,160 +1,91 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
-//Não constam as inicializações e declarações de constantes
-#define DO 262
-#define RE 294
-#define MI 330
-#define FA 349
-#define SOL 392
-#define LA 440
-#define SI 494
-#define DO2 523
+#define pinoDoBuzzer 21
+#define pinoDoPrimeiroLed 11
+#define pinoDoSegundoLed 12
+#define pinoDoTerceiroLed 13
 
-void musica1();
-void musica2();
-void notamusical(int frequencia, int duracao);
-void read_key(char key);
+void Beep(){
+    for (int i = 0; i < 100; i++) {
+        gpio_put(pinoDoBuzzer, 1);
+        sleep_us(500);
+        gpio_put(pinoDoBuzzer, 0);
+        sleep_us(500);
+    }
+}
 
-int main()
-{
-    stdio_init_all()
+int main() {
+    stdio_init_all();
     keypad_init();
-
-    while (true)
-    {
-        char key = read_keypad();
-        read_key(key);
-    }    
-        return 0;
-}
-
-void read_key(char key){
-switch (key)
-        {
-        case '1': //Buzzer emite um DÓ
-            notamusical(DO, 600);
-            printf("A tecla 1 foi pressionada. Tocando a nota DÓ.");
-            sleep_ms(200);
-            break;
-        case '2'://Buzzer emite um RÉ
-            notamusical(RE, 600);
-            printf("A tecla 2 foi pressionada. Tocando a nota RÉ.");
-            sleep_ms(200);
-            break;
-        case '3'://Buzzer emite um MI
-            notamusical(MI, 600);
-            printf("A tecla 3 foi pressionada. Tocando a nota MI");
-            sleep_ms(200);
-            break;
-        case '4'://Buzzer emite um FÁ
-            notamusical(FA, 600);
-            printf("A tecla 4 foi pressionada. Tocando a nota FÁ");
-            sleep_ms(200);
-            break;
-        case '5'://Buzzer emite um SOL
-            notamusical(SOL, 600);
-            printf("A tecla 5 foi pressionada. Tocando a nota SOL");
-            sleep_ms(200);
-            break;
-        case '6'://Buzzer emite um LÁ
-            notamusical(LA, 600);
-            printf("A tecla 6 foi pressionada. Tocando a nota LÁ");
-            sleep_ms(200);
-            break;
-        case '7'://Buzzer emite um SI
-            notamusical(SI, 600);
-            printf("A tecla 7 foi pressionada.  Tocando a nota SI");
-            sleep_ms(200);
-            break;
-        case '8'://Buzzer emite um DÓ
-            notamusical(DO2, 600);
-            printf("A tecla 8 foi pressionada. Tocando a nota DÓ");
-            sleep_ms(200);
-            break;
-        case '9'://Música toca
-            musica1();
-            printf("A tecla 9 foi pressionada. Música n°1 tocando.");
-            sleep_ms(200);
-            break;
-        case '0'://Outra música toca
-            musica2();
-            printf("A tecla 0 foi pressionada. Música n°2 tocando");
-            sleep_ms(200);
-            break;
-        case 'a'://liga verde
-            gpio_put(,1);
-            printf("A tecla a foi pressionada. Led verde ligado");
-            sleep_ms(200);
-            break;
-        case 'b'://liga o azul
-            gpio_put(,1);
-            printf("A tecla b foi pressionada. Led azul ligado");
-            sleep_ms(200);
-            break;
-        case 'c'://liga o vermelho
-            gpio_put(,1);
-            printf("A tecla c foi pressionada. Led vermelho ligado");
-            sleep_ms(200);
-            break;
-        case 'd'://alterna entre as três cores
-            while(1)
-            {
-                gpio_put(,1);
-                sleep_ms(300);
-                gpio_put(,0);
-                gpio_put(,1);
-                sleep_ms(300);
-                gpio_put(,0);
-                gpio_put(,1);
-                sleep_ms(300);
-                gpio_put(,0);
+    inicializarComponentes();       // Definindo o pino do 21 do buzzer como sa�da
+        while (true) {
+            char key = read_keypad();
+             if (key == '0') {  // Se uma tecla for pressionada
+                    gpio_put(pinoDoBuzzer, 1);
+                    sleep_ms(200);
+                    Beep();
             }
-            printf("A tecla d foi pressionada. Os três leds piscam de forma alternada");
-            sleep_ms(200);
-        break;
-        case '#'://Os três leds acendem
-            gpio_put(,1);
-            gpio_put(,1);
-            gpio_put(,1);
-            printf("A tecla # foi pressionada.");
-            sleep_ms(200);
-        case '*'://Buzzer liga
-            gpio_put(,1);
-            printf("A tecla * foi pressionada. Buzzer ligado.");
-            sleep_ms(200);
-            break;
-        default:
-            break;
+            else if (key=='1'){
+                gpio_put(pinoDoPrimeiroLed,1);
+                printf("\nTecla pressionada: 1. 1 Led ligado. \n");
+                sleep_ms(200);
+            }
+            else if (key=='2'){
+                gpio_put(pinoDoSegundoLed,1);
+                printf("\nTecla pressionada: 2. 1 Led ligado. \n");
+                sleep_ms(200);
+            }
+            else if (key=='3'){
+                gpio_put(pinoDoTerceiroLed,1);
+                printf("\nTecla pressionada: 3. 1 Led ligado. \n");
+                sleep_ms(200);
+            }
+            else if (key=='A'){
+                printf("\nTecla pressionada A. \n");
+                gpio_put(pinoDoPrimeiroLed,1);
+                sleep_ms(200);
+                gpio_put(pinoDoPrimeiroLed,0);
+                gpio_put(pinoDoSegundoLed,1);
+                sleep_ms(200);
+                gpio_put(pinoDoSegundoLed,0);
+                gpio_put(pinoDoTerceiroLed,1);
+                sleep_ms(200);
+                gpio_put(pinoDoTerceiroLed,0);
+                sleep_ms(200);
+           }
+            else if(key=='B'){
+                gpio_put(pinoDoPrimeiroLed,1);
+                gpio_put(pinoDoSegundoLed,1);
+                gpio_put(pinoDoTerceiroLed,1);
+                printf("\nTecla pressionada: B. Leds ligados. \n");
+                sleep_ms(200);
+           }
+            else if(key=='C'){
+                gpio_put(pinoDoPrimeiroLed,1);
+                gpio_put(pinoDoSegundoLed,0);
+                gpio_put(pinoDoTerceiroLed,1);
+                printf("\nTecla pressionada: C. 2 Leds ligados. \n");
+                sleep_ms(200);
+           } 
+           else if(key=='D'){
+                gpio_put(pinoDoPrimeiroLed,1);
+                gpio_put(pinoDoSegundoLed,1);
+                gpio_put(pinoDoTerceiroLed,0);
+                printf("\nTecla pressionada: D. 2 Leds ligados");
+                sleep_ms(200);
+           }
+            else if (key == '*'){
+                Beep();
+                Beep();
+                printf("\nTecla pressionada: *. Buzzer acionado. \n");
+                sleep_ms(200); 
+            }
+            else{
+                Beep();
+                printf("\nNenhuma tecla pressionada");
+                sleep_ms(200);
+            }
         }
-}
-
-void notamusical(int frequencia, int duracao) {
-    if (frequencia > 0) {
-        gpio_put(BUZZER_PIN, 1);
-        sleep_us(1000000 / frequencia);
-        gpio_put(BUZZER_PIN, 0);
-    }
-        sleep_ms(duracao);
-}
-
-void musica1() {
-    int notas[] = {262, 262, 392, 392, 440, 440, 392};  // Do, Do, Sol, Sol, La, La, Sol
-    int duracoes[] = {400, 400, 400, 400, 400, 400, 800};  // Duração das notas
-    for (int i = 0; i < 7; i++) {
-        notamusical(notas[i], duracoes[i]);
-    }
-}
-
-void musica2() {
-    int notas[] = {
-        392, 392, 440, 440, 392, 392, 349, 349, 330, 330, 294, 294, 262, 262, 294, 392  // Sol, Sol, La, La, Sol, Sol, Fa, Fa, Mi, Mi, Re, Re, Do, Do, Re, Sol
-    };
-    int duracoes[] = {
-        500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500  // Duração das notas
-    };
-    
-    for (int i = 0; i < 16; i++) {
-        notamusical(notas[i], duracoes[i]);
-    }
+        return 0;
 }
